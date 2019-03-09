190737b875f914b677fc71524a7a7986e0d61ee3
onatm <onatmercan@gmail.com>
2019-03-09 21:37:05 +0000

    implement basic arithmetic operations translation

--- src/translator/mod.rs
+++ src/translator/mod.rs
@@ -1,1 +1,44 @@
 pub struct Translator;
+
+use crate::parser::Arithmetic;
+use crate::parser::Operation;
+
+impl Translator {
+    pub fn translate(operations: &[Operation]) -> Vec<&str> {
+        let mut instructions: Vec<&str> = Vec::new();
+
+        for operation in operations {
+            match operation {
+                Operation::Arithmetic(arithmetic) => match arithmetic {
+                    Arithmetic::Add => {
+                        Translator::arithmetic_calculation_template(&mut instructions);
+                        instructions.push(&"M=M+D");
+                    }
+                    Arithmetic::Sub => {
+                        Translator::arithmetic_calculation_template(&mut instructions);
+                        instructions.push(&"M=M-D");
+                    }
+                    Arithmetic::And => {
+                        Translator::arithmetic_calculation_template(&mut instructions);
+                        instructions.push(&"M=M&D");
+                    }
+                    Arithmetic::Or => {
+                        Translator::arithmetic_calculation_template(&mut instructions);
+                        instructions.push(&"M=M|D");
+                    }
+                    _ => panic!("Supported arithmetic operations: (add|sub|and|or)"),
+                },
+                _ => (),
+            }
+        }
+
+        instructions
+    }
+
+    fn arithmetic_calculation_template(instructions: &mut Vec<&str>) {
+        instructions.push(&"@SP");
+        instructions.push(&"AM=M-1");
+        instructions.push(&"D=M");
+        instructions.push(&"A=A-1");
+    }
+}
